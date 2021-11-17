cbb2dcdcee83c7d7277c69f70399914a2f85e320
james-yw <yw1134324372@gmail.com>
2021-11-17 04:16:05 -0800

    init

---
+++ build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yang/scudb/src
+# Build directory: /home/yang/scudb/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
